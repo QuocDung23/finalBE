a0f0adc8cdd30fef87c2944f5d23080e74a1f846
dlthaonhi <thaonhi6102005ll@gmail.com>
2025-04-16 20:58:48 +0700

    feat/mongo-basic

---
+++ mongo_crud_app.js
@@ -0,0 +1,69 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+// MongoDB connection
+mongoose.connect('mongodb://user:password@127.0.0.1:27019/S-Mongo?authSource=admin', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+
+const movieSchema = new mongoose.Schema({
+  title: String,
+  genre: String,
+  rating: Number
+});
+
+// Create a model from the schema
+const Movie = mongoose.model('Movie', movieSchema);
+
+const app = express();
+app.use(express.json());
+
+// Create a new movie
+app.post('/movies', async (req, res) => {
+  const { title, genre, rating } = req.body;
+  const movie = new Movie({
+    title,
+    genre,
+    rating
+  });
+  try {
+    const result = await movieCollection.insertOne(movie);
+    res.status(201).send(result.ops[0]);
+  } catch (err) {
+    res.status(400).send('Error: ' + err.message);
+  }
+});
+
+// Read all movies
+app.get('/movies', async (req, res) => {
+  try {
+
+  } catch (err) {
+    res.status(400).send('Error: ' + err.message);
+  }
+});
+
+// Update a movie
+app.put('/movies/:id', async (req, res) => {
+  try {
+
+  } catch (err) {
+    res.status(400).send('Error: ' + err.message);
+  }
+});
+
+// Delete a movie
+app.delete('/movies/:id', async (req, res) => {
+
+  try {
+
+  } catch (err) {
+    res.status(400).send('Error: ' + err.message);
+  }
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
