721935b3216e5aecf432e9d00f13bc2fea063ce3
dlthaonhi <thaonhi6102005ll@gmail.com>
2025-04-16 21:23:52 +0700

    feat/mongo-basic

--- mongo_crud_app.js
+++ mongo_crud_app.js
@@ -13,7 +13,7 @@ const movieSchema = new mongoose.Schema({
 });
 
 // Create a model from the schema
-const Movie = mongoose.model('Movie', movieSchema);
+const movieCollection = mongoose.model('Movie', movieSchema);
 
 const app = express();
 app.use(express.json());
@@ -42,6 +42,38 @@ app.get('/movies', async (req, res) => {
 app.put('/movies/:id', async (req, res) => {
   try {
 
+  } catch (err) {
+    res.status(400).send('Error: ' + err.message);
+  }
+});
+
+app.put('/movies/:id', async (req, res) => {
+  const { id } = req.params;
+  const { title, genre, rating } = req.body;
+
+  try {
+    const result = await movieCollection.updateOne( //db.movie
+      { _id: new MongoClient.ObjectID(id) }, 
+      { $set: { title, genre, rating } }
+    );
+    if (result.matchedCount === 0) {
+      res.status(404).send('Movie not found');
+    } else {
+      res.status(200).send('Movie updated');
+    }
+  } catch (err) {
+    res.status(400).send('Error: ' + err.message);
+  }
+});
+
+// Update a movie
+app.put('/movies/:id', async (req, res) => {
+  const { id } = req.params;
+  const { title, genre, rating } = req.body;
+
+  try {
+    const movie = await Movie.findByIdAndUpdate(id, { title, genre, rating }, { new: true });
+    res.status(200).send(movie);
   } catch (err) {
     res.status(400).send('Error: ' + err.message);
   }
